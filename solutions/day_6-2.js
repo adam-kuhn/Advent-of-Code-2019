6d4b835e395bdc9ce6fc8c88aac13f4ac812ebe0
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-08 19:27:19 +0200

    solution day 6 part 2

---
+++ solutions/day_6-2.js
@@ -0,0 +1,98 @@
+const fs = require('fs')
+
+fs.readFile('../problem-data/day_6.txt', 'utf-8', (err, contents) => {
+  if (err) {
+    console.error(err)
+  }
+  const testData = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN']
+  const orbits = contents.split('\n')
+  const parsedOrbits = orbits.map(orbitPair => parseOrbits(orbitPair))
+  const firstOrbit = parsedOrbits.filter(orbits => orbits.centerBody === 'COM')[0]
+  const bodyYouAreOrbiting = parsedOrbits.filter(orbits => orbits.orbitingBody === 'YOU')[0]
+  const bodySantaIsOrbiting = parsedOrbits.filter(orbits => orbits.orbitingBody === 'SAN')[0]
+  const indexOfFirstOrbit = parsedOrbits.indexOf(firstOrbit)
+  parsedOrbits.splice(indexOfFirstOrbit, 1) // remove first orbit
+
+  const removeMappedOrbits = (childOrbits) => {
+    childOrbits.forEach(orbit => {
+      const indexOfOrbit = parsedOrbits.indexOf(orbit)
+      parsedOrbits.splice(indexOfOrbit, 1)
+    })
+  }
+  const mappedOrbits = {}
+  let levelFromCom = 0
+  const addPlanetToMap = (orbitingBodies) => {
+    levelFromCom++
+    mappedOrbits[levelFromCom] = orbitingBodies
+  }
+  mappedOrbits[levelFromCom] = [firstOrbit]
+  while (parsedOrbits.length) {
+    const orbitingBodies = mappedOrbits[levelFromCom]
+    let orbitsAtSameDistance = []
+    orbitingBodies.forEach(orbit => {
+      const orbistsAroundCurrentBody = parsedOrbits.filter(orbitPair => orbit.orbitingBody === orbitPair.centerBody)
+      orbitsAtSameDistance.push(orbistsAroundCurrentBody)
+    })
+    const flattenOrbits = orbitsAtSameDistance.flat()
+    addPlanetToMap(flattenOrbits)
+    removeMappedOrbits(flattenOrbits)
+  }
+  const yourPathFromCurrentPositionToCom = createPathFromEndToCom(mappedOrbits, bodyYouAreOrbiting)
+  const santasPathFromCurrentPositionToCom = createPathFromEndToCom(mappedOrbits, bodySantaIsOrbiting)
+  const firstCommonPlanetWithSanta = findCommonPlanet(yourPathFromCurrentPositionToCom, santasPathFromCurrentPositionToCom)
+  const yourPathFromCommonPoint = pathFromCommonPlanet(firstCommonPlanetWithSanta, yourPathFromCurrentPositionToCom)
+  const santasPathFromCommonPoint = pathFromCommonPlanet(firstCommonPlanetWithSanta, santasPathFromCurrentPositionToCom)
+
+  const planetTransferToMeetSanta = yourPathFromCommonPoint.length + santasPathFromCommonPoint.length - 2 // minus 2 because it's space in between not the planets themselves
+  console.log(planetTransferToMeetSanta) // 496
+})
+
+function pathFromCommonPlanet (commonPlanet, entirePath) {
+  const startOfIndividualPath = entirePath.find(orbitPair => orbitPair.centerBody === commonPlanet)
+  const indexOfCommonPlanet = entirePath.indexOf(startOfIndividualPath)
+  const individualPath = entirePath.slice(0, indexOfCommonPlanet + 1)
+  return individualPath
+}
+function findCommonPlanet (pathOne, pathTwo) {
+  let firstCommonPlanet
+  pathOne.some(orbitPair => {
+    const commonPlanet = pathTwo.filter(secondPair => orbitPair.centerBody === secondPair.centerBody)[0]
+    firstCommonPlanet = commonPlanet
+    return firstCommonPlanet
+  })
+  return firstCommonPlanet.centerBody
+}
+
+function parseOrbits (orbitPair) {
+  const indexOfDivide = orbitPair.indexOf(')')
+  const centerBody = orbitPair.slice(0, indexOfDivide)
+  const orbitingBody = orbitPair.slice(indexOfDivide + 1)
+  return {
+    centerBody,
+    orbitingBody
+  }
+}
+
+function createPathFromEndToCom (mappedOrbits, orbitingBody) {
+  const positionFromCom = findDistanceFromCom(mappedOrbits, orbitingBody)
+  let currentOrbitingBody = orbitingBody.centerBody
+  let path = [orbitingBody]
+  for (let level = positionFromCom - 1; level >= 0; level--) {
+    const planetOnThePath = mappedOrbits[level].filter(orbitPairs => currentOrbitingBody === orbitPairs.orbitingBody)[0]
+    path = [planetOnThePath, ...path]
+    currentOrbitingBody = planetOnThePath.centerBody
+  }
+  return path.reverse()
+}
+
+function findDistanceFromCom (mappedOrbitLevels, orbitingBody) {
+  const orbitlevels = Object.keys(mappedOrbitLevels)
+  let positionFromCom
+  orbitlevels.forEach(level => {
+    const placeInMap = mappedOrbitLevels[level].indexOf(orbitingBody)
+    if (placeInMap > -1) {
+      positionFromCom = level
+    }
+  })
+  return positionFromCom
+}
