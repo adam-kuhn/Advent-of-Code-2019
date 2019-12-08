727dd5e8e875bf4fb2f4aab48e41372c8defd6d8
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-08 22:27:21 +0200

    day 7 part 1 solution

---
+++ solutions/Day-7/increments.js
@@ -0,0 +1,8 @@
+const increments = {
+  INC_BY_2: 2,
+  INC_BY_3: 3,
+  INC_BY_4: 4,
+  DONT_INC: 0
+}
+
+module.exports = increments
---
+++ solutions/Day-7/op-codes.js
@@ -0,0 +1,13 @@
+const opCodes = {
+  OP_ADD: 1,
+  OP_MULTI: 2,
+  OP_WRITE: 3,
+  OP_OUTPUT: 4,
+  OP_JUMP_IF_TRUE: 5,
+  OP_JUMP_IF_FALSE: 6,
+  OP_LESS_THAN: 7,
+  OP_EQUALS: 8,
+  OP_END: 99
+}
+
+module.exports = opCodes
